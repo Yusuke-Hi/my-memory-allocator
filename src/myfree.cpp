5fcd1b3a758d9c2ad2427250348c75a4d6e828a9
yusuke <yuusuke29413@gmail.com>
2026-04-19 15:54:54 +0900

    [UPDATE] applied coalescing.

--- src/myfree.cpp
+++ src/myfree.cpp
@@ -1,6 +1,30 @@
+#include "coalesce.hpp"
 #include "my-memory-allocator.hpp"
 
 void myfree(void* payload) {
-  MemoryHeader* memory_header = GetMemoryHeader(payload);
-  memory_header->is_free = true;
+  auto* target = GetMemoryHeader(payload);
+  target->is_free = true;
+
+  // Coalescing
+  if (free_list == target) {
+    if (target->next && target->next->is_free) {
+      Coalesce(target, target->next);
+    }
+    return;
+  }
+
+  auto* current{free_list};
+  while (current && current->next) {
+    if (current->next == target) {
+      // coalesce from from the rear, if can.
+      if (target->next && target->next->is_free) {
+        Coalesce(target, target->next);
+      }
+      if (current->is_free) {
+        Coalesce(current, target);
+      }
+      break;
+    }
+    current = current->next;
+  }
 }
