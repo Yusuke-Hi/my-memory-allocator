83c2b3eee72909edfc271fb7a000b5be2bfbf688
yusuke <yuusuke29413@gmail.com>
2026-04-19 15:57:43 +0900

    [ADD] coalescing test.

--- test/my_allocator_test.cpp
+++ test/my_allocator_test.cpp
@@ -2,8 +2,70 @@
 
 #include "my-memory-allocator.hpp"
 
+constexpr size_t kBasicBlockSize{64};
+
 TEST(MyMallocTest, BasicAllocation) {
-  void* ptr = mymalloc(64);
-  EXPECT_NE(ptr, nullptr);
-  myfree(ptr);
+  void* p = mymalloc(kBasicBlockSize);
+  EXPECT_NE(p, nullptr);
+  myfree(p);
+}
+
+TEST(MyMallocTest, CoalesceNext) {
+  void* p1 = mymalloc(kBasicBlockSize);
+  void* p2 = mymalloc(kBasicBlockSize);
+  void* p3 = mymalloc(kBasicBlockSize);
+
+  myfree(p2);
+  myfree(p1);
+
+  // check address
+  void* p4 = mymalloc(2 * kBasicBlockSize);
+  EXPECT_EQ(p1, p4);
+  // check size
+  auto* mh4 = GetMemoryHeader(p4);
+  EXPECT_EQ(mh4->size, 2 * kBasicBlockSize);
+
+  myfree(p3);
+  myfree(p4);
+}
+
+TEST(MyMallocTest, CoalescePrev) {
+  void* p1 = mymalloc(kBasicBlockSize);
+  void* p2 = mymalloc(kBasicBlockSize);
+  void* p3 = mymalloc(kBasicBlockSize);
+
+  myfree(p1);
+  myfree(p2);
+
+  // check address
+  void* p4 = mymalloc(2 * kBasicBlockSize);
+  EXPECT_EQ(p1, p4);
+
+  // check size
+  auto* mh4 = GetMemoryHeader(p4);
+  EXPECT_EQ(mh4->size, 2 * kBasicBlockSize);
+
+  myfree(p3);
+  myfree(p4);
+}
+
+TEST(MyMallocTest, CoalesceBoth) {
+  void* p1 = mymalloc(kBasicBlockSize);
+  void* p2 = mymalloc(kBasicBlockSize);
+  void* p3 = mymalloc(kBasicBlockSize);
+  void* p4 = mymalloc(kBasicBlockSize);
+
+  myfree(p1);
+  myfree(p3);
+  myfree(p2);
+
+  // check address
+  void* p5 = mymalloc(3 * kBasicBlockSize);
+  EXPECT_EQ(p1, p5);
+  // check size
+  auto* mh5 = GetMemoryHeader(p5);
+  EXPECT_EQ(mh5->size, 3 * kBasicBlockSize);
+
+  myfree(p4);
+  myfree(p5);
 }
